f008f87c2e018793fc1cd521010077a4328fb891
reka_w <reka@rekacodes.com>
2021-06-04 00:49:27 -0400

    Shipped site at end of class

--- index.php
+++ index.php
@@ -20,12 +20,20 @@ get_header(); ?>
 	<div class="main-content">
 		<?php if ( have_posts() ): ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part('content', get_post_format()); ?>
+				<?php get_template_part('content-blog', get_post_format()); ?>
 			<?php endwhile; ?>
 		<?php endif; ?>
 	</div>
 
 	<?php get_sidebar(); ?>
 </section>
-
+	<nav id="navigation" class="container">
+		<div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
+		<div class="pagination">
+			<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				echo 'Page '.$paged.' of '.$wp_query->max_num_pages;
+			?>
+		</div>
+		<div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
+</nav>
 <?php get_footer(); ?>
